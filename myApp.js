b0e4e5cca49e34d480e3b95f161571d0370e0dff
Ernesto Garcia <ernestogarcia@Ernestos-MacBook-Air.local>
2024-01-20 16:53:59 -0500

    completed the MongoDB and Mongoose for full course on Back End Development and APIs at freeCodeCamp.org

--- myApp.js
+++ myApp.js
@@ -1,54 +1,141 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+// Connect to database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-let Person;
+const Schema = mongoose.Schema;
+
+//Define schema for "person"
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: { type: Number },
+  favoriteFoods: [String],
+});
+
+// Create the model for "Person" based on the schema
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const person = new Person({
+    name: "John Doee",
+    age: 30,
+    favoriteFoods: "Pizza",
+  });
+
+  person.save(function (err, data) {
+    done(null, data);
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  // Insert multiple documents based on the provided arrayOfPeople
+  Person.create(arrayOfPeople, (err, createdPeople) => {
+    if (err) {
+      console.error("Error creating people:", err);
+      done(err); // Pass the error to the callback
+    } else {
+      console.log("People created:", createdPeople);
+      done(null, createdPeople); // Pass the createdPeople to the callback
+    }
+  });
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (err, personFound) => {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, foundPerson) => {
+    if (err) console.log(err);
+    done(null, foundPerson);
+  });
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, (err, foundPerson) => {
+    if (err) console.log(err);
+    done(null, foundPerson);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById(personId, (err, personFound) => {
+    if (err) {
+      console.error("Error finding person:", err);
+    } else if (personFound) {
+      // Check if the person document was found
+      personFound.favoriteFoods.push(foodToAdd); // Push the new favorite food
+
+      personFound.save((err, updatedPerson) => {
+        if (err) {
+          console.error("Error saving updated person:", err);
+        } else {
+          console.log("Updated person:", updatedPerson);
+        }
+        done(null, updatedPerson);
+      });
+    } else {
+      console.log("Person not found"); // Handle the case where the person was not found
+    }
+  });
 };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
+  const update = { age: ageToSet };
+  const options = {
+    returnDocument: "before",
+    new: true,
+  };
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    update,
+    options,
+    (err, recordUpdated) => {
+      if (err) console.log(err);
+      done(null, recordUpdated);
+    }
+  );
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findOneAndDelete({ _id: personId }, (err, result) => {
+    if (err) console.log(err);
+    done(null, result);
+  });
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  Person.deleteMany({ name: nameToRemove }, (err, result) => {
+    if (err) console.log(err);
+    done(null, result);
+  });
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  const query = Person.find({ favoriteFoods: foodToSearch })
+    .limit(2)
+    .sort({ name: 1 })
+    .select({ age: 0 });
+
+  query.exec((err, result) => {
+    if (err) console.log(err);
+    done(null, result);
+  });
 };
 
 /** **Well Done !!**
